a8bf810de3d854cd3a93dd96587de340b8e9d24d
Mathieu <mathieu.ouvrard.ext@mappy.com>
2018-09-03 14:45:22 +0200

    IOMA-8249: add script to build swift sample

---
+++ scripts/build_ui.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+SIMULATOR_VERSION=11.4
+
+xcodebuild clean build -scheme "Example (Swift)" -destination "platform=iOS Simulator,name=iPhone 8,OS=${SIMULATOR_VERSION}" | xcpretty && exit ${PIPESTATUS[0]}
+
