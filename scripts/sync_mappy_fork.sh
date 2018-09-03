543e2c0a7ddd0fd1e2b8ba4797b6815f9c231191
Mathieu <mathieu.ouvrard.ext@mappy.com>
2018-09-03 15:15:44 +0200

    IOMA-8249: add script to sync Mappy fork

---
+++ scripts/sync_mappy_fork.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# sync_mappy_fork - A script to sync Mappy fork of Directions with Mapbox repository
+
+git remote -v | grep 'upstream' &> /dev/null || git remote add upstream https://github.com/mapbox/mapbox-gl-directions.git
+git remote update
+git checkout master
+git pull origin master
+git merge upstream/master
+git push origin master
+git checkout -
