37480b1db4f7a77ab3ce8ee7cadb7421f39b15df
Romain Quidet <romain.quidet.ext@mappy.com>
2018-09-14 11:33:22 +0200

    IOMA-8249: code review

--- scripts/sync_mappy_fork.sh
+++ scripts/sync_mappy_fork.sh
@@ -2,7 +2,7 @@
 
 # sync_mappy_fork - A script to sync Mappy fork of Directions with Mapbox repository
 
-git remote -v | grep 'upstream' &> /dev/null || git remote add upstream https://github.com/mapbox/mapbox-gl-directions.git
+git remote -v | grep 'upstream' &> /dev/null || git remote add upstream https://github.com/mapbox/MapboxDirections.swift.git
 git remote update
 git checkout master
 git pull origin master
