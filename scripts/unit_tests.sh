c1e73691b1c988e58d66e4fa2fb247f796f77c85
Mathieu <mathieu.ouvrard.ext@mappy.com>
2018-10-08 17:23:38 +0200

    [sh] pass to iOS 12 toolchain

--- scripts/unit_tests.sh
+++ scripts/unit_tests.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-SIMULATOR_VERSION=11.4
+SIMULATOR_VERSION=12.0
 
 xcodebuild test -scheme "MapboxDirections iOS" -destination "platform=iOS Simulator,name=iPhone 8,OS=${SIMULATOR_VERSION}" | xcpretty -r junit --output test-reports/TEST-MBDirections.xml && exit ${PIPESTATUS[0]}
 
