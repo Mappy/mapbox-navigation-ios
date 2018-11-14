7082e396be4824b76d2d805f9ac3b8cd16590316
Romain Quidet <romain.quidet.ext@mappy.com>
2018-11-14 16:40:29 +0100

    [Mappy] correct merge issues

--- scripts/build_ui.sh
+++ scripts/build_ui.sh
@@ -6,5 +6,5 @@ SIMULATOR_VERSION=12.1
 
 #xcodebuild clean build -scheme MapboxNavigation -destination "platform=iOS Simulator,name=iPhone 8,OS=${SIMULATOR_VERSION}" | xcpretty && exit ${PIPESTATUS[0]}
 
-xcodebuild clean build -scheme Example-Swift -destination "platform=iOS Simulator,name=iPhone 8,OS=${SIMULATOR_VERSION}" | xcpretty && exit ${PIPESTATUS[0]}
+xcodebuild clean build -scheme Example -destination "platform=iOS Simulator,name=iPhone 8,OS=${SIMULATOR_VERSION}" | xcpretty && exit ${PIPESTATUS[0]}
 
