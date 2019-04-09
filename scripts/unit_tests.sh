552823b5b9518ab26ad656501b0ba2e2cd21fea7
Jean-Baptiste Quesney <jean.baptiste.quesney.ext@mappy.com>
2019-04-09 09:50:22 +0200

    [XCode] IOMA-9043: Update to iOS 12.2 SDK

--- scripts/unit_tests.sh
+++ scripts/unit_tests.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-SIMULATOR_VERSION=12.1
+SIMULATOR_VERSION=12.2
 
 xcodebuild test -scheme MapboxCoreNavigation -destination "platform=iOS Simulator,name=iPhone 8,OS=${SIMULATOR_VERSION}" | xcpretty -r junit --output test-reports/TEST-MBCoreNavigation.xml && exit ${PIPESTATUS[0]}
 
