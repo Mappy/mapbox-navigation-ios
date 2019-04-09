e39b63e192ce3462895e775f24b4d60fef581504
Jean-Baptiste Quesney <jean.baptiste.quesney.ext@mappy.com>
2019-04-09 09:50:28 +0200

    [XCode] IOMA-9043: Update to iOS 12.2 SDK

--- scripts/build_ui.sh
+++ scripts/build_ui.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-SIMULATOR_VERSION=12.1
+SIMULATOR_VERSION=12.2
 
 xcodebuild clean build -scheme "Example (Swift)" -destination "platform=iOS Simulator,name=iPhone 8,OS=${SIMULATOR_VERSION}" | xcpretty && exit ${PIPESTATUS[0]}
 
--- scripts/unit_tests.sh
+++ scripts/unit_tests.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-SIMULATOR_VERSION=12.1
+SIMULATOR_VERSION=12.2
 
 xcodebuild test -scheme "MapboxDirections iOS" -destination "platform=iOS Simulator,name=iPhone 8,OS=${SIMULATOR_VERSION}" | xcpretty -r junit --output test-reports/TEST-MBDirections.xml && exit ${PIPESTATUS[0]}
 
