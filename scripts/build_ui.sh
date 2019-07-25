89bd5f4229a5d8df1bb65e5992550efcd538c27b
Jean-Baptiste Quesney <jean.baptiste.quesney.ext@mappy.com>
2019-07-25 15:20:19 +0200

    [Mappy] Don't hardcode OS version in build script.

--- scripts/build_ui.sh
+++ scripts/build_ui.sh
@@ -2,9 +2,7 @@
 
 set -e
 
-SIMULATOR_VERSION=12.2
+#xcodebuild clean build -scheme MapboxNavigation -destination "platform=iOS Simulator,name=iPhone 8" | xcpretty && exit ${PIPESTATUS[0]}
 
-#xcodebuild clean build -scheme MapboxNavigation -destination "platform=iOS Simulator,name=iPhone 8,OS=${SIMULATOR_VERSION}" | xcpretty && exit ${PIPESTATUS[0]}
-
-xcodebuild clean build -scheme Example -destination "platform=iOS Simulator,name=iPhone 8,OS=${SIMULATOR_VERSION}" | xcpretty && exit ${PIPESTATUS[0]}
+xcodebuild clean build -scheme Example -destination "platform=iOS Simulator,name=iPhone 8" | xcpretty && exit ${PIPESTATUS[0]}
 
